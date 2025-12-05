0fb026976f46ba380f223f1257d86b4ad5d5260c
Jackson Hainey <91702925+GitlianWarrior@users.noreply.github.com>
2025-12-05 15:56:32 -0500

    Initialize Spring Boot project for rare-cars-sales
    Set up a new Spring Boot application with Maven wrapper, project structure, and initial configuration. Includes basic dependencies for Spring Boot, JPA, Security, Thymeleaf, and test support. Adds .gitignore, .gitattributes, and initial application and test classes.

---
+++ source/rare-cars-sales/src/main/java/com/revline/rare_cars_sales/RareCarsSalesApplication.java
@@ -0,0 +1,13 @@
+package com.revline.rare_cars_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RareCarsSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RareCarsSalesApplication.class, args);
+	}
+
+}
