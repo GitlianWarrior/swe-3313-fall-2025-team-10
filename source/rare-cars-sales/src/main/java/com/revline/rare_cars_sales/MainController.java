8d7246be55f7a1bc4af1dada60eb30ed6de84412
ferrerkiwi <andresferrerangarita@gmail.com>
2026-06-04 17:32:39 -0400

    Remove unused import statement from MainController

--- source/rare-cars-sales/src/main/java/com/revline/rare_cars_sales/MainController.java
+++ source/rare-cars-sales/src/main/java/com/revline/rare_cars_sales/MainController.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
